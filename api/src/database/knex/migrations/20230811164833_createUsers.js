3297a77baad03e84e5f24345def1b60b0f01bceb
alan <alanwengrzee@gmail.com>
2024-07-08 19:29:26 -0300

    :sparkles: feat: migration users

--- api/src/database/knex/migrations/20230811164833_createUsers.js
+++ api/src/database/knex/migrations/20230811164833_createUsers.js
@@ -4,6 +4,8 @@ exports.up = knex => knex.schema.createTable("users", table => {
   table.text("email").notNullable();
   table.text("password").notNullable();
 
+  table.enum("role", ["customer", "admin"], { useNative: true, enumName: "roles" }).notNullable().defaultTo("customer");
+
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
 });
