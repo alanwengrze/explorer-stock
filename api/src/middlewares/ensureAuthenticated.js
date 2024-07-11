fd943c5880b247f771ed7d2241f4e90b933eadef
alan <alanwengrzee@gmail.com>
2024-07-11 03:21:17 -0300

    :recycle: refactor: change localStorage for cookie

--- api/src/middlewares/ensureAuthenticated.js
+++ api/src/middlewares/ensureAuthenticated.js
@@ -3,9 +3,9 @@ const AppError = require('../utils/AppError');
 const authConfig = require('../configs/auth');
 
 function ensureAuthenticated(request, response, next) {
-  const authHeader = request.headers.authorization;
+  const authHeader = request.headers;
 
-  if (!authHeader) {
+  if (!authHeader.cookie) {
     throw new AppError('JWT token não informado', 401);
   }
 
